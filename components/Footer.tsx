af1be57d798de18cbee2f083c1a7526693561861
Muhammad Aslam <aslamkhan8012@gmail.com>
2023-03-12 02:31:13 +0500

    footer changes

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,10 +6,10 @@ const Footer = () => {
             <div className="max-w-4xl px-4 mx-auto text-gray-800 dark:text-white">
                 <div className="pb-8 mb-2 border-t-2 border-gray-300 dark:border-white-300"/>
                 <div className="flex flex-col justify-between lg:flex-row items-center">
-                    <p>Built with Next.js, Tailwind</p>
+                    <p>Built by M. Aslam</p>
                     <div className="flex flex-wrap pt-2 sm:space-x-4 space-x-2 font-medium lg:pt-0">
                         <a
-                            href="https://twitter.com/paulknulst"
+                            href="https://twitter.com/Aslamraisani55"
                             className={"transition-colors hover:text-yellow-500"}
                             target="_blank"
                             rel="noreferrer"
@@ -17,7 +17,7 @@ const Footer = () => {
                             Twitter
                         </a>
                         <a
-                            href="https://www.linkedin.com/in/paulknulst/"
+                            href="https://www.linkedin.com/in/aslamraisani/"
                             className={"transition-colors hover:text-yellow-500"}
                             target="_blank"
                             rel="noreferrer"
@@ -25,21 +25,14 @@ const Footer = () => {
                             LinkedIn
                         </a>
                         <a
-                            href="https://github.com/paulknulst"
+                            href="https://github.com/aslamkhan5"
                             className={"transition-colors hover:text-yellow-500"}
                             target="_blank"
                             rel="noreferrer"
                         >
                             GitHub
                         </a>
-                        <a
-                            href="https://blog.paulknulst.de"
-                            className={"transition-colors hover:text-yellow-500"}
-                            target="_blank"
-                            rel="noreferrer"
-                        >
-                            Medium
-                        </a>
+                       
                     </div>
                 </div>
             </div>
