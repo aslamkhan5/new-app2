7b228ca8540574cf476065a23b42e223f4df600f
Muhammad Aslam <aslamkhan8012@gmail.com>
2023-03-12 09:31:59 +0500

    3rd commit

--- components/About.tsx
+++ components/About.tsx
@@ -4,7 +4,7 @@ import profile from "../public/1604.jpeg"
 
 const About = () => {
     return (
-        <div className="container px-4 mx-auto">
+        <div className="container px-4 py-4 mx-auto">
             <div
                 className="lg:space-x-5 lg:flex lg:flex-row item-center lg:-mx-4 flex flex-col-reverse text-center lg:text-left">
                 <div className="lg:px-4 lg:mt-12 ">
