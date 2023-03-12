20e20f7fbad5971aca0debf88b254a8cc6e34162
Muhammad Aslam <aslamkhan8012@gmail.com>
2023-03-12 13:23:22 +0500

    name changeddfd

--- components/About.tsx
+++ components/About.tsx
@@ -9,7 +9,7 @@ const About = () => {
                 className="lg:space-x-5 lg:flex lg:flex-row item-center lg:-mx-4 flex flex-col-reverse text-center lg:text-left">
                 <div className="lg:px-4 lg:mt-12 ">
                     <h1 className="text-2xl font-bold text-gray-900 lg:text-5xl dark:text-white">
-                        Hey there,
+                        Hello there,
                     </h1>
                     <div className="mt-6 text-gray-800 dark:text-white">
                         <p className="mb-4">I'm a Tech enthusiast & a Fullstack Developer. I work as a Team Lead at <a
